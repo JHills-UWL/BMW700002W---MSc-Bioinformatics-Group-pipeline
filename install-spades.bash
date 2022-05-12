ec60a9576019cf40dafbfb2ed2375234ffb92a8f
JHills-UWL <93379494+JHills-UWL@users.noreply.github.com>
2022-05-12 19:44:41 +0100

    Delete install-spades.bash

--- install-spades.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-wget https://cab.spbu.ru/files/release3.15.4/SPAdes-3.15.4-Linux.tar.gz
-tar -xf SPAdes-3.15.4-Linux.tar.gz
-rm SPAdes-3.15.4-Linux.tar.gz
-sudo ln -s $PWD/SPAdes-3.15.4-Linux/bin/spades.py /usr/local/bin
